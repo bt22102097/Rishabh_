1b66efe0f215e16a2be23231ff8311cee9260015
bt22102097 <131702945+bt22102097@users.noreply.github.com>
2023-04-24 23:44:31 +0530

    Add files via upload

---
+++ Answer 2...py
@@ -0,0 +1,7 @@
+name="Rishabh Tripathi"
+department="Civil"
+SID= str(22102097)
+CGPA= str(8.5)
+print("Hey,"+name+" here")
+print("My SID is "+ SID)
+print("I am from "+department+" department and my CGPA is "+CGPA)
---
+++ Answer 3...py
@@ -0,0 +1,15 @@
+a=56
+b=10
+print(a&b)
+print(a|b)
+print(a**b)
+res = a<<2
+print(res)
+
+res = b<<2
+print(res)
+res = a>>2
+print(res)
+
+res= b>>4
+print(res)
